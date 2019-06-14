31147bdfe4967ceb2d740b44024203d8c7266673
front-end-jeko <43757639+front-end-jeko@users.noreply.github.com>
2019-06-14 11:46:35 +0400

    update javascirpt

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.news__slider').slick({
-        // autoplay: true,
+        autoplay: true,
         dots: true,
         prevArrow: '<img class="slider__arrow left__arrow" src="images/arrow_left.png" alt="left arrow">',
         nextArrow: '<img class="slider__arrow right__arrow" src="images/arrow_right.png" alt="left arrow">',
