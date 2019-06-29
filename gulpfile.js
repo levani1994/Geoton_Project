336d769d7b5c51d0670626c39280d6b4753c86cb
Konstantine Kokochashvili <43369878+Kokochashvili@users.noreply.github.com>
2019-06-29 15:44:18 +0400

    update theme

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('sass', function() {
   return gulp.src('scss/style.scss')
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(autoprefixer({
-      browsers: ['last 20 versions'],
+      browsers: ['last 10 versions'],
       cascade: false
     }))
     .pipe(gulp.dest('css'));
